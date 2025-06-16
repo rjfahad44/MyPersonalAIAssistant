6b41286251d9c2bb53dc291bc058e5e2c684bde4
Fahad <rjfahad44@gmail.com>
2025-06-16 14:38:04 +0600

    update LLamaAndroid.kt load() func

--- llama/src/main/java/android/llama/cpp/LLamaAndroid.kt
+++ llama/src/main/java/android/llama/cpp/LLamaAndroid.kt
@@ -103,7 +103,7 @@ class LLamaAndroid {
                     val context = new_context(model)
                     if (context == 0L) throw IllegalStateException("new_context() failed")
 
-                    val batch = new_batch(512, 0, 1)
+                    val batch = new_batch(2048, 0, 1) //512
                     if (batch == 0L) throw IllegalStateException("new_batch() failed")
 
                     val sampler = new_sampler()
